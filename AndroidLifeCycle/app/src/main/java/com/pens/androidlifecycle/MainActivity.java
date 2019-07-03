06ccf7ccdbc6644d74f51c56237c1ed88073de57
zulhaydarakbar <fyrozal.akbar@gmail.com>
2019-07-03 15:39:18 +0700

    Log
    add Log

--- AndroidLifeCycle/app/src/main/java/com/pens/androidlifecycle/MainActivity.java
+++ AndroidLifeCycle/app/src/main/java/com/pens/androidlifecycle/MainActivity.java
@@ -2,26 +2,65 @@ package com.pens.androidlifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"MainActivity onCreate");
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(this,"App on Start",Toast.LENGTH_SHORT).show();
+        Log.d(TAG,"MainActivity onStart");
+        Toast.makeText(this, "App on Start", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(this,"App on Stop",Toast.LENGTH_SHORT).show();
+        Log.d(TAG,"MainActivity onStop");
+        Toast.makeText(this, "App on Stop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"MainActivity onPause");
+        Toast.makeText(this, "App on Pause", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"MainActivity onResume");
+        Toast.makeText(this, "App on Resume", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"MainActivity onRestart");
+        Toast.makeText(this, "App on Restart", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"MainActivity onDestroy");
+        Toast.makeText(this, "App on Destroy", Toast.LENGTH_SHORT).show();
+
     }
 
     /*
